4fbfc9dc8ed00b36578c63032bd7a7eff272136a
fabian.k <fabian.k@unimus.net>
2026-03-10 13:55:10 +0100

    f

--- components/desfire_reader/__init__.py
+++ components/desfire_reader/__init__.py
@@ -1,48 +1,71 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import pn532
+from esphome.components import i2c, text_sensor, binary_sensor
 from esphome.const import CONF_ID
 
-AUTO_LOAD = ["pn532"]
+AUTO_LOAD = ["text_sensor", "binary_sensor"]
 
 desfire_ns = cg.esphome_ns.namespace("desfire_reader")
-DesfireReader = desfire_ns.class_("DesfireReader", cg.Component)
+DesfireReaderComponent = desfire_ns.class_(
+    "DesfireReaderComponent",
+    cg.PollingComponent,
+    i2c.I2CDevice,
+)
 
-CONF_PN532_ID = "pn532_id"
-CONF_APPLICATION_ID = "application_id"
-CONF_FILE_ID = "file_id"
-CONF_KEY_ID = "key_id"
-CONF_AUTHENTICATION_KEY = "authentication_key"
-CONF_DATA_KEY = "data_key"
+CONF_APP_ID   = "app_id"
+CONF_APP_KEY  = "app_key"   # AES-128 key for app authentication
+CONF_DATA_KEY = "data_key"  # AES-128 key to decrypt file contents
+CONF_RESULT   = "result"
+CONF_AUTH_OK  = "auth_ok"
+CONF_UID      = "uid"
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(DesfireReader),
 
-        cv.Required(CONF_PN532_ID): cv.use_id(pn532.PN532),
+def validate_hex_bytes(expected_len, label):
+    def _validate(value):
+        parts = str(value).split(":")
+        if len(parts) != expected_len:
+            raise cv.Invalid(f"{label} must be {expected_len} bytes (XX:XX format)")
+        try:
+            return [int(p, 16) for p in parts]
+        except ValueError:
+            raise cv.Invalid(f"{label}: invalid hex '{value}'")
+    return _validate
 
-        cv.Required(CONF_APPLICATION_ID): cv.hex_int,
-        cv.Required(CONF_FILE_ID): cv.int_range(min=0, max=31),
-        cv.Required(CONF_KEY_ID): cv.int_range(min=0, max=13),
 
-        cv.Required(CONF_AUTHENTICATION_KEY): cv.string,
-        cv.Optional(CONF_DATA_KEY): cv.string,
-    }
-).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = (
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(DesfireReaderComponent),
+        cv.Required(CONF_APP_ID):   validate_hex_bytes(3,  "app_id"),
+        cv.Required(CONF_APP_KEY):  validate_hex_bytes(16, "app_key"),
+        cv.Required(CONF_DATA_KEY): validate_hex_bytes(16, "data_key"),
+        cv.Optional(CONF_RESULT):   text_sensor.text_sensor_schema(),
+        cv.Optional(CONF_AUTH_OK):  binary_sensor.binary_sensor_schema(),
+        cv.Optional(CONF_UID):      text_sensor.text_sensor_schema(),
+    })
+    .extend(i2c.i2c_device_schema(0x24))
+    .extend(cv.polling_component_schema("500ms"))
+)
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
 
-    pn = await cg.get_variable(config[CONF_PN532_ID])
-    cg.add(var.set_pn532(pn))
+    aid = config[CONF_APP_ID]
+    cg.add(var.set_app_id(aid[0], aid[1], aid[2]))
 
-    cg.add(var.set_application_id(config[CONF_APPLICATION_ID]))
-    cg.add(var.set_file_id(config[CONF_FILE_ID]))
-    cg.add(var.set_key_id(config[CONF_KEY_ID]))
+    cg.add(var.set_app_key(config[CONF_APP_KEY]))
+    cg.add(var.set_data_key(config[CONF_DATA_KEY]))
 
-    cg.add(var.set_authentication_key(config[CONF_AUTHENTICATION_KEY]))
+    if CONF_RESULT in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_RESULT])
+        cg.add(var.set_result_sensor(sens))
 
-    if CONF_DATA_KEY in config:
-        cg.add(var.set_data_key(config[CONF_DATA_KEY]))
+    if CONF_AUTH_OK in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_AUTH_OK])
+        cg.add(var.set_auth_sensor(sens))
+
+    if CONF_UID in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_UID])
+        cg.add(var.set_uid_sensor(sens))
